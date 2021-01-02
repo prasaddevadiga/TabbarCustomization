03232de5a9b199cda45fbe1fca71ca9eca313db6
Prasad Devadiga <prasad>
2021-01-02 12:36:59 +0530

    Fixed tabbar arc

--- TabbarCustomization/Customize/CustomTabbar.swift
+++ TabbarCustomization/Customize/CustomTabbar.swift
@@ -7,17 +7,13 @@
 
 import UIKit
 
+let tabBarHeight: CGFloat = 100.0
+
 @IBDesignable
 class CustomTabbar: UITabBar {
 
     private var shapeLayer: CAShapeLayer?
     
-    
-    
-    
-    
-//     Only override draw() if you perform custom drawing.
-//     An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         addShape()
     }
@@ -31,65 +27,24 @@ class CustomTabbar: UITabBar {
         shapeLayer.shadowOpacity = 0.1
         shapeLayer.shadowRadius = 15.0
         
-        let squareLayer = CAShapeLayer()
-        squareLayer.path = createSquarePath()
-        squareLayer.fillColor = UIColor.white.cgColor
-        
         if let oldLayer = self.shapeLayer {
             self.layer.replaceSublayer(oldLayer, with: shapeLayer)
         } else {
             self.layer.insertSublayer(shapeLayer, at: 0)
-            self.layer.insertSublayer(squareLayer, at: 0)
         }
         self.shapeLayer = shapeLayer
     }
     
     override func sizeThatFits(_ size: CGSize) -> CGSize {
         var size = super.sizeThatFits(size)
-        size.height = 100
+        size.height = tabBarHeight
         return size
     }
     
-    func createSquarePath() -> CGPath {
-        let path = UIBezierPath.init(roundedRect: CGRect(x: self.bounds.origin.x, y: self.bounds.height/2-10, width: self.bounds.width, height: self.bounds.height),
-                                      byRoundingCorners: UIRectCorner(rawValue: UInt(0.0)),
-                                      cornerRadii: CGSize(width: self.bounds.width, height: self.bounds.height))
-        return path.cgPath
-    }
-    
     func createArchPath() -> CGPath {
-        let path = UIBezierPath.init(roundedRect: CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y - 10, width: self.bounds.width, height: self.bounds.height),
-                                      byRoundingCorners: UIRectCorner(rawValue: UInt(22.0)),
-                                      cornerRadii: CGSize(width: self.bounds.width, height: self.bounds.height))
-        return path.cgPath
-        
-//        let path = UIBezierPath()
-//        path.move(to: CGPoint(x: 30, y: 10))
-//        path.addLine(to: CGPoint(x: self.frame.width-30, y: 10))
-//        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
-//        path.addLine(to: CGPoint(x: 0 , y: self.frame.height))
-//        path.close()
-//        return path.cgPath
-
-    }
-    
-    func createPath() -> CGPath {
-        let height: CGFloat = 37.0
-        let path = UIBezierPath()
-        let centerWidth: CGFloat = self.frame.size.width/2
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: (centerWidth - height * 2) , y: 0))
-        
-        path.addCurve(to: CGPoint(x: centerWidth, y: height),
-                      controlPoint1: CGPoint(x: (centerWidth - 30), y: 0),
-                      controlPoint2: CGPoint(x: centerWidth-35, y: height))
-        path.addCurve(to: CGPoint(x: (centerWidth + height * 2), y:0),
-                      controlPoint1: CGPoint(x: centerWidth + 35, y: height),
-                      controlPoint2: CGPoint(x: (centerWidth + 30), y: 0))
-        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
-        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
-        path.addLine(to: CGPoint(x: 0 , y: self.frame.height))
-        path.close()
+        let path = UIBezierPath.init(roundedRect: self.bounds,
+                                     byRoundingCorners: [.topLeft ,.topRight],
+                                      cornerRadii: CGSize(width:tabBarHeight/2, height:tabBarHeight/2))
         return path.cgPath
     }
 }
