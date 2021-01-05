9ce9baa322ac9c61e09b02f02ce7499b03f556b9
Prasad Devadiga <prasad>
2021-01-05 22:33:45 +0530

    Fixed animation

--- TabbarCustomization/ViewControllers/TopImageTableViewCell.swift
+++ TabbarCustomization/ViewControllers/TopImageTableViewCell.swift
@@ -13,9 +13,14 @@ class TopImageTableViewCell: UITableViewCell {
     @IBOutlet weak var ratingView: RatingView!
     @IBOutlet weak var ratingBarRightConstraint: NSLayoutConstraint!
     @IBOutlet weak var ratingBarLeftConstraint: NSLayoutConstraint!
+    let shapeLayer = CAShapeLayer()
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.ratingView.alpha = 0.0
+        shapeLayer.path = getCollapsePath()
+        self.topImageView.layer.mask = shapeLayer
+        
         maskTopImage()
         presentAnimation()
     }
@@ -25,22 +30,26 @@ class TopImageTableViewCell: UITableViewCell {
         UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseOut, animations: {
             self.ratingBarLeftConstraint.constant = 40
             self.ratingBarRightConstraint.constant = 40
+            self.ratingView.alpha = 1.0
             self.layoutIfNeeded()
         }, completion: nil)
     }
     
     func dismissAnimation(completion: @escaping ((Bool) -> Void)) {
-        
-        UIView.animate(withDuration: 0.15, delay: 0.0, options: .curveEaseIn, animations: {
+        imageCollapseAnimation()
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .transitionCrossDissolve, animations: {
             self.ratingBarLeftConstraint.constant = 140
             self.ratingBarRightConstraint.constant = 140
+            self.ratingView.alpha = 0.0
             self.layoutIfNeeded()
+            
         }, completion: {(sucess) in
             completion(sucess)
         })
     }
     
     func maskTopImage() {
+        imageExpandAnimation()
         
         let gradient = CAGradientLayer()
         gradient.frame = self.topImageView.bounds
@@ -48,12 +57,51 @@ class TopImageTableViewCell: UITableViewCell {
                            UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor,
                            UIColor.clear.cgColor]
         self.topImageView.layer.addSublayer(gradient)
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = UIBezierPath.init(roundedRect: topImageView.bounds,
-                                     byRoundingCorners: [.bottomLeft ,.bottomRight],
-                                     cornerRadii: CGSize(width:50, height:25)).cgPath
-        self.topImageView.layer.mask = shapeLayer
     }
 
+    func imageExpandAnimation() {
+        
+        let revealAnimation = CABasicAnimation(keyPath: "path")
+        revealAnimation.fromValue = getCollapsePath()
+        revealAnimation.toValue = exppansionPath()
+        revealAnimation.duration = 0.15
+        revealAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        shapeLayer.add(revealAnimation, forKey: nil)
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        shapeLayer.path = exppansionPath()
+        CATransaction.commit()
+        
+    }
+    
+    func imageCollapseAnimation() {
+        let revealAnimation = CABasicAnimation(keyPath: "path")
+        revealAnimation.fromValue = exppansionPath()
+        revealAnimation.toValue = getCollapsePath()
+        revealAnimation.duration = 0.15
+        revealAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        shapeLayer.add(revealAnimation, forKey: nil)
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        shapeLayer.path = exppansionPath()
+        CATransaction.commit()
+    }
+    
+    func getCollapsePath() -> CGPath {
+        return UIBezierPath.init(roundedRect: CGRect(origin: topImageView.frame.origin,
+                                                     size: CGSize(width: topImageView.bounds.width,
+                                                                  height: topImageView.bounds.height/4)),
+                          byRoundingCorners: [.bottomLeft ,.bottomRight],
+                          cornerRadii: CGSize(width:50, height:50)).cgPath
+    }
+    
+    func exppansionPath() -> CGPath {
+        return UIBezierPath.init(roundedRect: topImageView.bounds,
+                                 byRoundingCorners: [.bottomLeft ,.bottomRight],
+                                 cornerRadii: CGSize(width:50, height:50)).cgPath
+    }
 }
